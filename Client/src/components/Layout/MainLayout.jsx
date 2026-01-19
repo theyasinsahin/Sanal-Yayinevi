a33b78fae0811f19c7ad44c7e852ec6f7bbfe475
theyasinsahin <yasin248sahin@gmail.com>
2026-01-19 04:42:27 +0300

    chore(structure): add Layout folder for new atomic design folder structure

---
+++ Client/src/components/Layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavigationBar from '../../components/Navigation/NavigationBar'; // Navbar'ı buraya taşıyacağız
+import Footer from '../../components/Navigation/Footer'; // Footer yapınca buraya
+
+export const MainLayout = ({ children }) => {
+  return (
+    <div className="main-layout">
+      <NavigationBar />
+      <main className="main-content">
+        {children}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
